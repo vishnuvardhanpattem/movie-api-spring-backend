266947cca633acbb309885abb510738a9bb1a41d
vishnuvardhanpattem <97816318+vishnuvardhanpattem@users.noreply.github.com>
2024-03-10 10:04:59 +0530

    Add files via upload

---
+++ src/main/java/com/vishnu/movies/CorsConfig.java
@@ -0,0 +1,51 @@
+//package com.vishnu.movies;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//	@Bean
+//	public CorsFilter corsFilter() {
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		CorsConfiguration config = new CorsConfiguration();
+//		config.addAllowedOrigin("http://localhost:3000", "https://splendorous-sundae-37c6f1.netlify.app/"); // Replace
+//																											// with the
+//																											// actual
+//																											// origin of
+//																											// your
+//																											// React app
+//		config.addAllowedMethod("*");
+//		config.addAllowedHeader("*");
+//		source.registerCorsConfiguration("/**", config);
+//		return new CorsFilter(source);
+//	}
+//}
+
+package com.vishnu.movies;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("http://localhost:3000");
+		config.addAllowedOrigin("https://splendorous-sundae-37c6f1.netlify.app"); // Remove the trailing slash
+		config.addAllowedMethod("*");
+		config.addAllowedHeader("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+}
